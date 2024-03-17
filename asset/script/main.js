6062dae4a7fcf371fbd64d73131f632910a4c406
ai.hirose <h7tm2a@outlook.jp>
2024-03-18 06:49:02 +0900

    最新

--- asset/script/main.js
+++ asset/script/main.js
@@ -1,11 +1,30 @@
-/* header
------------------------------------------- */
 $('.hamburger-button').on('click', function (e) {
   $(".hamburger-menu").slideToggle();
 });
 
-$('.hamburger-menu a[href^="#"]').on('click', function () {
+$('.hamburger-menu a[href^="#"]').click(function(){
+  var speed = 500;
+  var href= $(this).attr("href");
+  var target = $(href == "#" || href == "" ? 'html' : href);
+  var header = $('header').height();
+  var position = target.offset().top - header;
+  $("html, body").animate({scrollTop:position}, speed, "swing");
+  return false;
+});
+
+$('.hamburger-menu a[href^="#"]').on('click', function() {
   $('.hamburger-button').click();
+  return false;
+});
+
+$('.header-nav a[href^="#"]').click(function(){
+  var speed = 500;
+  var href= $(this).attr("href");
+  var target = $(href == "#" || href == "" ? 'html' : href);
+  var header = $('header').height();
+  var position = target.offset().top - header;
+  $("html, body").animate({scrollTop:position}, speed, "swing");
+  return false;
 });
 
 /* course
