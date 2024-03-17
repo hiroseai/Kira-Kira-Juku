79fb749968a72acb48a81c5b20d09205f8dbb10c
ai.hirose <h7tm2a@outlook.jp>
2024-03-17 13:57:23 +0900

    first commit

---
+++ asset/script/main.js
@@ -0,0 +1,15 @@
+/* header
+------------------------------------------ */
+$('.hamburger-button').on('click', function (e) {
+  $(".hamburger-menu").slideToggle();
+});
+
+$('.hamburger-menu a[href^="#"]').on('click', function () {
+  $('.hamburger-button').click();
+});
+
+/* course
+------------------------------------------ */
+$('#smarttab').smartTab({
+  enableUrlHash: false // タブidにもとづいたURLハッシュの付与を無効
+});
